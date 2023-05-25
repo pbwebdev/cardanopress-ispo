9e17b2c8f251892b41343037d858148a9103d57b
kermage <genealyson.torcende@gmail.com>
2023-05-25 11:46:52 +0800

    log every step and handle possible bad requests

--- src/Exporter.php
+++ src/Exporter.php
@@ -38,6 +38,7 @@ class Exporter implements HookInterface
             add_query_arg(
                 [
                     'action' => self::ACTION,
+                    'nonce' => wp_create_nonce(self::ACTION),
                     'network' => $network,
                 ],
                 admin_url('admin-ajax.php')
@@ -48,22 +49,60 @@ class Exporter implements HookInterface
 
     public function doAction(): void
     {
+        if (! wp_verify_nonce($_REQUEST['nonce'] ?? '', self::ACTION)) {
+            $this->dieHandler('forbidden');
+        }
+
+        $network = sanitize_key($_GET['network'] ?? '');
+
+        if (empty($network)) {
+            $this->dieHandler('unknown');
+        }
+
+        $pools = Application::getInstance()->option('delegation_pool_id');
+        $pool_id = $pools[$network] ?? '';
+
+        if (empty($pool_id)) {
+            wp_die(__('Unknown pool to process', 'cardanopress-ispo'));
+        }
 
         try {
             $csv = Writer::createFromStream(tmpfile());
 
+            $this->log(__METHOD__ . ' ' . $pool_id);
             $csv->insertOne(['stake_address', 'lovelace_amount', 'calculated_reward']);
-            $csv->insertAll($this->getData($_GET['network']));
+            $csv->insertAll($this->getData($network, $pool_id));
             $csv->output(date('Y-m-d-H-i') . '-data.csv');
         } catch (Exception $exception) {
             $this->log($exception->getMessage());
         }
+
+        $this->dieHandler('success');
     }
 
-    protected function getData(string $network): array
+    protected function dieHandler(string $type)
+    {
+        $types = [
+            'success' => [
+                'message' => '',
+                'code' => 200,
+            ],
+            'unknown' => [
+                'message' => __('Unknown network to process', 'cardanopress-ispo'),
+                'code' => 400,
+            ],
+            'forbidden' => [
+                'message' => __('Invalid nonce value provided', 'cardanopress-ispo'),
+                'code' => 403,
+            ],
+        ];
+
+        wp_die($types[$type]['message'], __('Export Data', 'cardanopress-ispo'), $types[$type]['code']);
+    }
+
+    protected function getData(string $network, string $pool_id): array
     {
         $application = Application::getInstance();
-        $pools = $application->option('delegation_pool_id');
         $ration = $application->option('rewards_ration');
         $multiplier = $application->option('rewards_multiplier');
         $blockfrost = new Blockfrost($network);
@@ -71,7 +110,9 @@ class Exporter implements HookInterface
         $page = 1;
 
         do {
-            $response = $this->requestData($blockfrost, $pools[$network], $page);
+            $response = $this->requestData($blockfrost, $pool_id, $page);
+
+            $this->log(__METHOD__ . ' count=' . count($response));
 
             foreach ($response as $delegation) {
                 $stake_address = $delegation['address'];
@@ -88,6 +129,8 @@ class Exporter implements HookInterface
 
     protected function requestData(Blockfrost $blockfrost, string $pool_id, int $page): array
     {
+        $this->log(__METHOD__ . ' page #' . $page);
+
         $response = $blockfrost->request('pools/' . $pool_id . '/delegators', compact('page'));
 
         return 200 === $response['status_code'] ? $response['data'] : [];
