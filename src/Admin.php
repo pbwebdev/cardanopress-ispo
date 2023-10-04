c4991d565a49dbdde59fccfaebfd8f7ec1c1a680
kermage <genealyson.torcende@gmail.com>
2023-10-04 12:31:21 +0800

    updated fields for multi-pool

--- src/Admin.php
+++ src/Admin.php
@@ -30,20 +30,116 @@ class Admin extends AbstractAdmin
             $this->generalSettings();
             $this->delegationSettings();
             $this->rewardsSettings();
+            $this->poolSettings();
         });
+
+        add_action('themeplate_settings_cp-ispo_advanced', [$this, 'customStyle']);
+    }
+
+    protected function generalFields(): array
+    {
+        return [
+            'allocated_tokens' => [
+                'title' => __('Allocated Tokens', 'cardanopress-ispo'),
+                'type' => 'number',
+                'default' => '',
+            ],
+        ];
+    }
+
+    protected function delegationFields(): array
+    {
+        return [
+            'pool_id' => [
+                'title' => __('Bech32 ID', 'cardanopress-ispo'),
+                'type' => 'group',
+                'default' => [
+                    'mainnet' => '',
+                    'testnet' => '',
+                ],
+                'fields' => [
+                    'mainnet' => [
+                        'title' => __('Mainnet', 'cardanopress-ispo'),
+                        'type' => 'text',
+                        'required' => true,
+                    ],
+                    'testnet' => [
+                        'title' => __('Testnet', 'cardanopress-ispo'),
+                        'type' => 'text',
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    protected function rewardsFields(): array
+    {
+        return [
+            'ration' => [
+                'title' => __('Ratio Per ADA', 'cardanopress-ispo'),
+                'description' => __('In percentage', 'cardanopress-ispo'),
+                'type' => 'number',
+                'default' => 1,
+                'options' => [
+                    'min' => 0.001,
+                    'max' => 100,
+                    'step' => 0.001,
+                ],
+            ],
+            'minimum' => [
+                'title' => __('Minimum ADA', 'cardanopress-ispo'),
+                'type' => 'number',
+                'default' => 1,
+                'options' => [
+                    'min' => 1,
+                    'step' => 1,
+                ],
+            ],
+            'maximum' => [
+                'title' => __('Maximum ADA', 'cardanopress-ispo'),
+                'type' => 'number',
+                'default' => 2,
+                'options' => [
+                    'min' => 2,
+                    'step' => 1,
+                ],
+            ],
+            'commence' => [
+                'title' => __('Commence at Epoch', 'cardanopress-ispo'),
+                'type' => 'number',
+                'default' => 1,
+                'options' => [
+                    'min' => 1,
+                    'step' => 1,
+                ],
+            ],
+            'conclude' => [
+                'title' => __('Conclude at Epoch', 'cardanopress-ispo'),
+                'type' => 'number',
+                'default' => 2,
+                'options' => [
+                    'min' => 2,
+                    'step' => 1,
+                ],
+            ],
+            'multiplier' => [
+                'title' => __('Loyalty multiplier', 'cardanopress-ispo'),
+                'description' => __('Every qualifying epoch', 'cardanopress-ispo'),
+                'type' => 'number',
+                'default' => 1,
+                'options' => [
+                    'min' => 1,
+                    'step' => 0.001,
+                ],
+            ],
+        ];
     }
 
     public function generalSettings(): void
     {
         $this->optionFields(__('General Settings', 'cardanopress-ispo'), [
             'data_prefix' => '',
-            'fields' => [
-                'allocated_tokens' => [
-                    'title' => __('Allocated Tokens', 'cardanopress-ispo'),
-                    'type' => 'number',
-                    'default' => '',
-                ],
-            ],
+            'fields' => $this->generalFields(),
         ]);
     }
 
@@ -51,26 +147,7 @@ class Admin extends AbstractAdmin
     {
         $this->optionFields(__('Delegation Settings', 'cardanopress-ispo'), [
             'data_prefix' => 'delegation_',
-            'fields' => [
-                'pool_id' => [
-                    'type' => 'group',
-                    'default' => [
-                        'mainnet' => '',
-                        'testnet' => '',
-                    ],
-                    'fields' => [
-                        'mainnet' => [
-                            'title' => __('Mainnet', 'cardanopress-ispo'),
-                            'type' => 'text',
-                            'required' => true,
-                        ],
-                        'testnet' => [
-                            'title' => __('Testnet', 'cardanopress-ispo'),
-                            'type' => 'text',
-                        ],
-                    ],
-                ],
-            ],
+            'fields' => $this->delegationFields(),
         ]);
     }
 
@@ -78,65 +155,38 @@ class Admin extends AbstractAdmin
     {
         $this->optionFields(__('Rewards Settings', 'cardanopress-ispo'), [
             'data_prefix' => 'rewards_',
+            'fields' => $this->rewardsFields(),
+        ]);
+    }
+
+    public function poolSettings(): void
+    {
+        $this->optionFields(__('Pool Settings', 'cardanopress-ispo'), [
+            'data_prefix' => '',
             'fields' => [
-                'ration' => [
-                    'title' => __('Ratio Per ADA', 'cardanopress-ispo'),
-                    'description' => __('In percentage', 'cardanopress-ispo'),
-                    'type' => 'number',
-                    'default' => 1,
-                    'options' => [
-                        'min' => 0.001,
-                        'max' => 100,
-                        'step' => 0.001,
-                    ],
-                ],
-                'minimum' => [
-                    'title' => __('Minimum ADA', 'cardanopress-ispo'),
-                    'type' => 'number',
-                    'default' => 1,
-                    'options' => [
-                        'min' => 1,
-                        'step' => 1,
-                    ],
-                ],
-                'maximum' => [
-                    'title' => __('Maximum ADA', 'cardanopress-ispo'),
-                    'type' => 'number',
-                    'default' => 2,
-                    'options' => [
-                        'min' => 2,
-                        'step' => 1,
-                    ],
-                ],
-                'commence' => [
-                    'title' => __('Commence at Epoch', 'cardanopress-ispo'),
-                    'type' => 'number',
-                    'default' => 1,
-                    'options' => [
-                        'min' => 1,
-                        'step' => 1,
-                    ],
-                ],
-                'conclude' => [
-                    'title' => __('Conclude at Epoch', 'cardanopress-ispo'),
-                    'type' => 'number',
-                    'default' => 2,
-                    'options' => [
-                        'min' => 2,
-                        'step' => 1,
-                    ],
-                ],
-                'multiplier' => [
-                    'title' => __('Loyalty multiplier', 'cardanopress-ispo'),
-                    'description' => __('Every qualifying epoch', 'cardanopress-ispo'),
-                    'type' => 'number',
-                    'default' => 1,
-                    'options' => [
-                        'min' => 1,
-                        'step' => 0.001,
-                    ],
+                'settings' => [
+                    'type' => 'group',
+                    'style' => 'ispo-pool-settings',
+                    'repeatable' => true,
+                    'fields' => $this->delegationFields() + $this->generalFields() + $this->rewardsFields(),
                 ],
             ],
         ]);
+    }
+
+    public function customStyle(): void
+    {
+        ob_start(); ?>
+
+        <style>
+            .ispo-pool-settings .themeplate-clone .type-group {
+                margin-top: 0;
+                margin-left: 1em;
+                margin-right: 1em;
+            }
+        </style>
+
+        <?php
+        echo wp_kses(ob_get_clean(), ['style' => []]);
     }
 }
