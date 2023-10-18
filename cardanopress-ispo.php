34e7c20b834c537deaac9ac743632bdc908353be
kermage <genealyson.torcende@gmail.com>
2023-10-18 10:26:36 +0800

    bump version to 1.3.1

--- cardanopress-ispo.php
+++ cardanopress-ispo.php
@@ -6,7 +6,7 @@
  * Author:      Gene Alyson Fortunado Torcende
  * Author URI:  https://cardanopress.io
  * Description: A CardanoPress extension for ISPO
- * Version:     1.3.0
+ * Version:     1.3.1
  * License:     GPL-2.0-only
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  *
